45dce2a13e1ad3430bd581b3d769c39ca73caada
colewilldev <wcole2222@gmail.com>
2020-12-08 19:03:51 -0700

    fixed issues with Product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,33 @@
 import data from "./data";
-console.log(data.products);
+import { BrowserRouter, Route } from "react-router-dom";
+
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="index.html">
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/{${product._id}`}>
-                <img
-                  className="img medium"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
-              <div className="card-body">
-                <a href="proudct.html">
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">
-        <p>All Rights Reserved 2021</p>
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+        </main>
+        <footer className="row center">
+          <p>All Rights Reserved 2021</p>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
